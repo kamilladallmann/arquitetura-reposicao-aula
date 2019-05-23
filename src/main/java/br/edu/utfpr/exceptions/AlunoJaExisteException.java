bf4f92bdf39c982b657fefdfb9cdc3fd414e7b43
evandrocf <vandin23cedrick@gmail.com>
2019-05-23 20:36:34 -0300

    Anotações do projeto, code review

--- src/main/java/br/edu/utfpr/exceptions/AlunoJaExisteException.java
+++ src/main/java/br/edu/utfpr/exceptions/AlunoJaExisteException.java
@@ -1,5 +1,9 @@
 package br.edu.utfpr.exceptions;
-
+/**
+ * As classes de exceção apenas chamam a classe superior,
+ * sem implementações específicas. Não possui parâmetro Throwable,
+ * fazendo com que se perca a causa do erro.
+ */
 public class AlunoJaExisteException extends Exception {
     public AlunoJaExisteException (String msg){
         super(msg);
